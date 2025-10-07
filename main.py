549395c30360ee59f753323af2ed87ce95981ca5
CristiAbraham26 <acristianrazvan@gmail.com>
2025-10-07 13:40:46 +0300

    cristi
    bau

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 print("Salut")
 print("Hello, World!")
 print("ceva")
-<<<<<<< HEAD
-
-
 print("Adela")
-=======
 print('acum')
-<<<<<<< Updated upstream
-
-=======
 print("salut sunt cristi")
 print(27**8)
->>>>>>> Stashed changes
->>>>>>> a899addecb7ccad5ba5ffe2a67784b33841aec73
+
