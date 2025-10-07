bfcff77038dfe6309bb88f5754e61447df7de71a
MorarLorena27 <lorenamorar2006@gmail.com>
2025-10-07 13:42:01 +0300

    nhgd

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 print("Salut")
 print("Hello, World!")
 print("ceva")
-<<<<<<< HEAD
+
 
 
 print("Adela")
-=======
-print('acum')
-<<<<<<< Updated upstream
 
-=======
+print('acum')
+
+
+
 print("salut sunt cristi")
 print(27**8)
 print("Adela")
->>>>>>> Stashed changes
->>>>>>> a899addecb7ccad5ba5ffe2a67784b33841aec73
+
+print('Lorena')
