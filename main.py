65505302a85082bb2aa63aa098d6a1ad8263c5fb
Bogdan <bogdanioanteglas@yahoo.com>
2025-10-07 13:53:29 +0300

    cleanup

--- main.py
+++ main.py
@@ -1,41 +1,13 @@
 print("Salut")
 print("Hello, World!")
 print("ceva")
-<<<<<<< HEAD
 print("buna")
-=======
-<<<<<<< HEAD
-<<<<<<< Updated upstream
 print('acum')
-=======
->>>>>>> Stashed changes
-
 print("Lavi")
-=======
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-
-
 print("Adela")
-=======
 print('acum')
-<<<<<<< Updated upstream
-
-=======
 print("salut sunt cristi")
 print(27**8)
 print("Adela")
->>>>>>> Stashed changes
->>>>>>> a899addecb7ccad5ba5ffe2a67784b33841aec73
->>>>>>> 8030d72c98f06a2541f95981a9c62e99bc19179b
-<<<<<<< HEAD
-
-=======
->>>>>>> 625411de2cffe03b3f748b155f59dc04815b7e11
-
 print("hello")
->>>>>>> fe98c336f31720fe1ed4c5a795673e02911ed4a9
-
 print('lorena')
