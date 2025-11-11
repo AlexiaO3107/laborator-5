d34ed587d86ea85f94673fa1fd167696020caed4
blehhhh <ardeleanionut10@gmail.com>
2025-11-11 09:09:16 +0200

    lab5

--- laborator_5/functii_studenti.py
+++ laborator_5/functii_studenti.py
@@ -7,5 +7,5 @@ def sumacif(n):
         while n!=0:
             s = s+(n%10)
             n//=10
-    print (f"Suma cifrelor lui {c} este {s}")
+    print (f"Suma cifrelor lui {c} este {s}.")
 sumacif(1234)
