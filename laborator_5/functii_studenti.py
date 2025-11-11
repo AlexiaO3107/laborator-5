37b253afe4e8ec0b29d6e365c59928758a7903ae
Bogdan <bogdanioanteglas@yahoo.com>
2025-11-11 09:23:59 +0200

    Revert "Update functii_studenti.py"
    This reverts commit 8fe8f188d1537f0e377a55a3a9f4f496e9053d99.

--- laborator_5/functii_studenti.py
+++ laborator_5/functii_studenti.py
@@ -1,25 +1,21 @@
-#FUNCTIA 5
-def lista_prime_pana_la(n: int) -> str:
-    prime = []
+#Functie Filtreaza Pare (6)
+def filtreaza_pare(lista):
+    ok=0
+    for numar in lista:
+        if numar%2==0:
+            print(numar)
+            ok=1
+    if ok==0:
+        print("Nu exista numere pare.")
 
-    #  pana la 2
-    for numar in range(2, n + 1):
-        este_prim = True  # daca prim
-
-        # verificam
-        for divizor in range(2, numar):
-            if numar % divizor == 0:
-                este_prim = False
-                break
-
-        # daca prim
-        if este_prim:
-            prime.append(str(numar))
-
-    if len(prime) == 0:
-        return f"Nu există numere prime până la {n}."
+def sumacif(n):
+    s = 0
+    c=n
+    if n == 0:
+        return 0
     else:
-        return f"Numere prime până la {n}: " + ", ".join(prime)     # daca gol
-
-
-print(lista_prime_pana_la(20))
+        while n!=0:
+            s = s+(n%10)
+            n//=10
+    print (f"Suma cifrelor lui {c} este {s}")
+sumacif(1234)
