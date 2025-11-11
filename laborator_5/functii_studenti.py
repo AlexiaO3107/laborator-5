458bac209e51f186994925719f626803a6ed7183
PMiro06 <popmiroslav1999@gmail.com>
2025-11-11 08:56:47 +0200

    .

---
+++ laborator_5/functii_studenti.py
@@ -0,0 +1,9 @@
+#Functie Filtreaza Pare (6)
+def filtreaza_pare(lista):
+    ok=0
+    for numar in lista:
+        if numar%2==0:
+            print(numar)
+            ok=1
+    if ok==0:
+        print("Nu exista numere pare")
